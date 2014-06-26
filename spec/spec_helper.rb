fe2823b3ef7649daf9a6fc41f1aac986ad73df7c
Dmitriy Ugnichenko <du@anahoret.com>
2014-06-26 15:58:33 +0300

    Require capybara webkit for using driver.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'capybara/rspec'
+require 'capybara-webkit'
 
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
