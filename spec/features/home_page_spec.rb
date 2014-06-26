9c1071df804e1de9bbc7e88153e0ea1f09203f48
Dmitriy Ugnichenko <du@anahoret.com>
2014-06-26 18:11:58 +0300

    Get rid of obsolete option.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,4 +1,4 @@
-feature ['Home page'], type: :feature, js: true do
+feature ['Home page'], js: true do
   scenario 'Visit home page' do
     visit root_path
     expect(page).to have_content('Welcome to HelpDesc System!')
