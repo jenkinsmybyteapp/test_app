a6c88172b1f2727d28ce63d7ccde3c53b6162bcc
Dmitriy Ugnichenko <du@anahoret.com>
2014-06-27 13:27:52 +0300

    Add integration tests for ticket submission.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 feature ['Home page'], js: true do
-  scenario 'Visit home page' do
+  backround do
     visit root_path
+  end
+
+  scenario 'Visit home page' do
     expect(page).to have_content('Welcome to HelpDesk System!')
     expect(page).to have_css(('textarea[name="ticket[body]"]'))
+  end
+  describe 'Tickets submission' do
+    scenario 'successfull' do
+      fill_in 'Issue', with: 'Cannot understand this crazy world'
+      click_button 'Create Ticket'
+      wait_for_ajax
+      expect(page).to have_content('Your ticket has been successfully submitted')
+    end
+
+    scenario 'with empty body' do
+      click_button 'Create Ticket'
+      wait_for_ajax
+      expect(page).to have_content('Error! Please fill in some ticket text')
+    end
   end
 end
