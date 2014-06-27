82c9fbf0fdb121d7925cc7ba595e283d20efe260
Dmitriy Ugnichenko <du@anahoret.com>
2014-06-27 13:12:15 +0300

    Add tickets form, apply styles and fix specs.

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,5 @@
 class HomePageController < ApplicationController
   def index
+    @ticket = Ticket.new
   end
 end
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature ['Home page'], js: true do
   scenario 'Visit home page' do
     visit root_path
-    expect(page).to have_content('Welcome to HelpDesc System!')
+    expect(page).to have_content('Welcome to HelpDesk System!')
+    expect(page).to have_css(('textarea[name="ticket[body]"]'))
   end
 end
