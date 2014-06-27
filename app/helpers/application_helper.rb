0aad4d2fcdde8dddcd19d77afd9dd75a1792af6d
Dmitriy Ugnichenko <du@anahoret.com>
2014-06-27 15:00:53 +0300

    Add mechanism to display top flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class_name(name)
+    case name
+      when :notice then 'info'
+      when :success then 'success'
+      when :error then 'danger'
+    else
+      'warning'
+    end
+  end
 end
