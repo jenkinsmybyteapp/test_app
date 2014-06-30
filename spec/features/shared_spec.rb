89fdeb096c9f739b469ea441b69306590f3cd8a7
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-06-30 14:20:50 +0300

    Add spec for flash messages block.

---
+++ spec/features/shared_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature ['Shared functionality'], js: true do
+  describe 'Flash messages block' do
+    scenario 'Hides on click to close button' do
+      visit root_path
+      click_button 'Create Ticket'
+      wait_for_ajax
+      page.find('a[data-dismiss=alert]').click
+      expect(page).to have_css('.flash-messages-block', visible: false)
+    end
+  end
+end
