2ddc9d807d0645c155701eb2101978d94a6d8989
Dmitriy Ugnichenko <du@anahoret.com>
2014-07-01 11:50:20 +0300

    Change ticket reference generation mechanism. Set to_params to reference.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,17 +3,21 @@ class Ticket < ActiveRecord::Base
   before_create :generate_reference
   has_many :screenshots
 
+  def to_param
+    reference
+  end
+
   private
   def generate_reference
-    reference = random_str('A'..'Z') + random_str('0'..'9') +
-                random_str('A'..'Z') + random_str('0'..'9') +
-                random_str('A'..'Z')
+    reference = [ random_str('A'..'Z'), random_str('0'..'9'),
+                  random_str('A'..'Z'), random_str('0'..'9'),
+                  random_str('A'..'Z') ].join('-')
     self.reference = reference
   end
 
   def random_str(range, length = 3)
     return '' if length < 1 || range.count < length
 
-    range.to_a.shuffle[0, length].join
+    range.to_a.shuffle[0, length].join()
   end
 end
