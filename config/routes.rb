222ab75cdbd60936d1ce32bee0920bd992ffab2b
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-07-22 23:24:27 +0300

    Add scaffolding for dashboard controller.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
     resources :tickets, only: [:create, :show]
   end
 
+  scope module: :staff do
+    resources :dashboard, only: [:index]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
