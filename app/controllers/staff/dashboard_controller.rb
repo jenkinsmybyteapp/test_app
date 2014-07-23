acc1a8c46264c74df49a41fab4cc0c5b6824e542
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-07-24 02:42:57 +0300

    Display tickets list.

--- app/controllers/staff/dashboard_controller.rb
+++ app/controllers/staff/dashboard_controller.rb
@@ -2,9 +2,9 @@ class Staff::DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @open_tickets = Ticket.open
-    @hold_tickets = Ticket.hold
-    @closed_tickets = Ticket.closed
+    @open_tickets = Ticket.includes(:department).open
+    @hold_tickets = Ticket.includes(:department).hold
+    @closed_tickets = Ticket.includes(:department).closed
   end
 
 end
