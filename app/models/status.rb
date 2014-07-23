f4f927896241e1ef76cdc08b65fcbcf7d441a2d3
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-07-24 01:13:33 +0300

    Apply DRY principles.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,12 @@
 class Status < ActiveRecord::Base
   validates :name, presence: true
   has_one :ticket
+
+  CLOSED_STATUSES = ['Cancelled', 'Completed']
+  OPEN_STATUSES = ['Waiting for Staff Response', 'Waiting for Customer']
+  HOLD_STATUSES = ['On Hold']
+
+  scope :closed, -> { where('name IN (?)', CLOSED_STATUSES) }
+  scope :open, -> { where('name IN (?)', OPEN_STATUSES) }
+  scope :hold, -> { where('name IN (?)', HOLD_STATUSES) }
 end
