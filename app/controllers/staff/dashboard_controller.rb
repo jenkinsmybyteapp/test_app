b6cb412a47394399d761da860321c3b5700f3474
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-07-24 02:16:21 +0300

    Base scaffolding for ticket tabs.

--- app/controllers/staff/dashboard_controller.rb
+++ app/controllers/staff/dashboard_controller.rb
@@ -2,6 +2,9 @@ class Staff::DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @open_tickets = Ticket.open
+    @hold_tickets = Ticket.hold
+    @closed_tickets = Ticket.closed
   end
 
 end
