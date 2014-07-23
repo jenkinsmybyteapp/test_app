00341ffcbc182d1818747df5590f7187b9854db1
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-07-24 00:26:51 +0300

    Fix tests.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 feature ['Home page'], js: true do
+  let!(:department) { create :department }
+  let!(:status) { create :status }
+
   background do
     visit root_path
   end
@@ -9,9 +12,12 @@ feature ['Home page'], js: true do
     expect(page).to have_content('Welcome to HelpDesk System!')
     expect(page).to have_css(('textarea[name="ticket[body]"]'))
   end
+
   describe 'Tickets submission' do
     scenario 'successfull' do
       fill_in 'Issue', with: 'Cannot understand this crazy world'
+      fill_in 'ticket_email', with: 'sample@example.com'
+      select department.name, from: 'ticket_department_id'
       click_button 'Create Ticket'
       wait_for_ajax
       expect(page).to have_content('Your ticket has been successfully submitted')
